54a9355d65e8c8a9b20f7a51c60d2d4361f7fa68
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-13 14:56:36 -0500

    Ejercicio 4

---
+++ todo_Sobre_Variables_FechaActual_03_09_2022/main.js
@@ -0,0 +1,16 @@
+
+// 4.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript en el <head></ head>, 
+// que has de grabar en una carpeta llamada todo_Sobre_Variables_FechaActual, y declarar variable y asignar a cada una de 
+// ellas un valor de tipo String,Number,Booleano y mostrar los valores por medio de la consola o de la ventana emergente, 
+// Nota la variable de tipo constante const el tipo de valor es opcional.
+
+let caracter = 'Hola Mundo';
+let numero = 5;
+let valor = false;
+const constante = 60;
+
+console.log("El String es ", caracter);
+console.log("El número es ", numero);
+console.log("El valor del booleano es ", valor);
+console.log("La constante es ", constante);
+
