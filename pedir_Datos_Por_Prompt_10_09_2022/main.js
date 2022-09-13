1b7837fa2c891ad5fea72f8be0328bd6007d5d75
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-13 15:08:42 -0500

    Ejercicio 5

---
+++ pedir_Datos_Por_Prompt_10_09_2022/main.js
@@ -0,0 +1,11 @@
+
+// 5.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript 
+// en el <head></ head>, que has de grabar en una carpeta llamada pedir_Datos_Por_Prompt_FechaActual, 
+// En la ventana saldrá un texto que diga “Ingresar un numero” capturar el valor en un variable y 
+// mostrar el valor por medio de la consola o de la ventana emergente.
+
+let edad = prompt("Ingrese su edad", 0);
+
+console.log("La edad ingresada es ", edad);
+
+
